9d3db7e892e5ac645415a26d140551d7265690a4
Fidel Mordzinu <109281167+fidelmordzinu@users.noreply.github.com>
2022-08-01 14:02:22 +0000

    New commit

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,6 +6,7 @@ import { Audio, AVPlaybackStatus } from "expo-av";
 
 const HomeScreen = () => {
   const [recording, setRecording] = React.useState();
+  const [pausing, setPausing] = React.useState(true);
 
   async function startRecording() {
     try {
@@ -21,6 +22,7 @@ const HomeScreen = () => {
         Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
       );
       setRecording(recording);
+      setPausing(false);
       console.log("Recording started");
     } catch (err) {
       console.error("Failed to start recording", err);
@@ -28,11 +30,10 @@ const HomeScreen = () => {
   }
 
   async function stopPausing() {
-    console.log("Stopping recording..");
-    setRecording(undefined);
-    await recording.stopAndUnloadAsync();
-    const uri = recording.getURI();
-    console.log("Recording stopped and stored at", uri);
+    console.log("Pausing recording..");
+    await recording.pauseAsync();
+    setPausing(true);
+    console.log("Recording paused", await recording.getStatusAsync());
   }
 
   async function stopRecording() {
@@ -44,7 +45,7 @@ const HomeScreen = () => {
   }
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View className="flex-1 justify-center items-center bg-stone-800 ">
+      <View className="flex-1 justify-center items-center bg-white ">
         <View className="flex-2 p-5 items-end justify-end w-full flex-row space-x-2">
           <Icon name="access-time" type="material" color="white" size={30} />
           <Icon name="more-vert" type="material" color="white" size={30} />
@@ -65,39 +66,61 @@ const HomeScreen = () => {
             >
               <View className="bg-red-600 flex-1 w-full h-full rounded-full justify-center items-center">
                 <Icon
-                  name={recording ? "pause" : "microphone"}
+                  name={!pausing ? "pause" : "microphone"}
                   type="font-awesome"
                   color="white"
                   size={35}
                 />
               </View>
             </Pressable>
+            {recording ? (
+              <View className="flex-row justify-around w-full">
+                <Pressable
+                  onPress={stopRecording}
+                  style={({ pressed }) => [
+                    {
+                      backgroundColor: pressed ? "red" : "#133132d1",
+                    },
+                    styles.pressableSmall,
+                  ]}
+                >
+                  <Icon
+                    name="play"
+                    type="font-awesome"
+                    color="white"
+                    size={24}
+                  />
+                </Pressable>
+                <Pressable
+                  onPress={stopRecording}
+                  style={({ pressed }) => [
+                    {
+                      backgroundColor: pressed ? "red" : "#133132d1",
+                    },
+                    styles.pressableSmall,
+                  ]}
+                >
+                  <Icon
+                    name="square"
+                    type="font-awesome"
+                    color="white"
+                    size={24}
+                  />
+                </Pressable>
+              </View>
+            ) : null}
           </View>
         </View>
-        {recording ? (
-          <View className="flex-2 p-5 justify-around flex-row w-full ">
-            <Pressable
-              onPress={stopRecording}
-              style={({ pressed }) => [
-                {
-                  backgroundColor: pressed ? "red" : "#133132d1",
-                },
-                styles.pressableSmall,
-              ]}
-            >
-              <Icon name="square" type="font-awesome" color="white" size={30} />
-            </Pressable>
-          </View>
-        ) : (
+        {recording ? null : (
           <View className="flex-2 p-5 justify-end flex-row w-full ">
             <View className="flex-row space-x-2">
               <Icon
                 name="library-music"
                 type="material"
-                color="white"
+                color="black"
                 size={25}
               />
-              <Text className="text-white text-lg">Recording</Text>
+              <Text className="text-black text-lg">Recording</Text>
             </View>
           </View>
         )}
@@ -119,10 +142,10 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 80,
+    borderRadius: 60,
     padding: 10,
-    width: 80,
-    height: 80,
+    width: 60,
+    height: 60,
   },
 });
 export default HomeScreen;
